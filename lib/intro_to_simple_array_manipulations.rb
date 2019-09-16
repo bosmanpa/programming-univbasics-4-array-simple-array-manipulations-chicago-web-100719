64ef98e60bce1e8523b0071671b7ba0bcb132e0f
Patrick Bosman <bsmn.ptrck@gmail.com>
2019-09-16 03:51:20 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -39,6 +39,6 @@ def using_delete(instructors, name)
   no_offense_steven = instructors.delete(name)
 end
 
-def using_delete_at(famous_robots, Integer)
-  deleted_robot = famous_robots.delete(Integer)
+def using_delete_at(famous_robots, i)
+  deleted_robot = famous_robots.delete_at(i)
 end
